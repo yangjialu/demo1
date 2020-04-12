5f494d39a4a029f8e628c68ea4a471a1f32cd2e1
yangjialu <573669898@qq.com>
2020-04-12 17:18:21 +0800

    add1

--- shot.py
+++ shot.py
@@ -7,12 +7,7 @@ if __name__ == '__main__':
     d = u2.connect()
 
     d.screenshot("main1"+".png")
-    # print(d.serial)
-    # print(d.wlan_ip)
-    # print(d.device_info)
-    # d.screen_on()
-    # d.screen_off()
-    # d.unlock()
+    print("第一次")
 
 
 
